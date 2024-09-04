7797bdbb75bc689457f3b7e0d05cc58208f50827
Monoramen <monoramens@gmail.com>
2024-09-04 17:33:41 +0300

    Change main class examples

--- src/main/java/com/a2/Main.java
+++ src/main/java/com/a2/Main.java
@@ -1,18 +1,17 @@
 package com.a2;
 
 
-import java.util.ArrayList;
-import java.util.HashMap;
-
 public class Main {
     public static void main(String[] args) {
         GenHashMap<Integer, String> list = new GenHashMap<>();
         System.out.println(list.entrySet());
         list.put(1, "One");
         list.put(2, "Two");
-        System.out.println(list.entrySet());
-
-        list.put(2,"Two1");
+        list.put(3, "Three");
+        list.put(4, "Four");
+        list.put(5, "Five");
+        list.put(6, "Six");
+        list.put(7, "Seven");
         System.out.println(list.entrySet());
         list.clear();
         System.out.println(list.keyset());
